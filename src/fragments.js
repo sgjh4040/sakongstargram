44feeec4284882a23e72b2a9c5d172d54c45a658
sgjh4040 <sgjh4040@naver.com>
2019-09-28 18:46:14 +0900

    update seeRoom

--- src/fragments.js
+++ src/fragments.js
@@ -1,6 +1,7 @@
 export const USER_FRAGMENT = `
     id
     username
+    avatar
 `  
 export const COMMENT_FRAGMENT = `
         id
@@ -9,6 +10,16 @@ export const COMMENT_FRAGMENT = `
             username
         }
 `;
+export const MESSAGE_FRAGMENT = `
+        id
+        text
+        to{
+            ${USER_FRAGMENT}
+        }
+        from{
+            ${USER_FRAGMENT}
+        }
+`
 
 export const FILE_FRAGMENT = `
         id
@@ -36,7 +47,10 @@ export const ROOM_FRAGMENT = `
     fragment RoomParts on Room{
         id
         participants{
-            id
+            ${USER_FRAGMENT}
+        }
+        messages{
+            ${MESSAGE_FRAGMENT}
         }
     }
 `;
