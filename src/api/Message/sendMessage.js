32a21c48a6c6d2764c319752bb516c1aee5d4bfd
sgjh4040 <sgjh4040@naver.com>
2019-09-28 18:16:09 +0900

    update sendMessage

---
+++ src/api/Message/sendMessage.js
@@ -0,0 +1,47 @@
+import { prisma } from "../../../generated/prisma-client";
+import {ROOM_FRAGMENT} from "../../fragments";
+export default {
+    Mutation:{
+        sendMessage: async (_,args,{request,isAuthenticated})=>{
+            isAuthenticated(request);
+            const {user} =request;
+            const {roomId,message,toId}= args;
+            let room;
+            if(roomId ===undefined){
+                console.log("요기1")
+                if(user.id !== toId){
+                    room = await prisma.createRoom({
+                        participants:{
+                            connect: [{id:toId},{id:user.id}]
+                        }
+                    }).$fragment(ROOM_FRAGMENT);
+                }
+            }else{
+                console.log("요기2")
+                room = await prisma.room({id:roomId}).$fragment(ROOM_FRAGMENT);
+                if(!room){
+                    throw Error("방을 찾지 못했습니다.")
+                }
+            }
+            console.log(room)
+            const getTo = room.participants.filter(participant => participant.id !==user.id)[0];
+            return prisma.createMessage({
+                text:message,
+                from: {
+                    connect: {id:user.id}
+                },
+                to:{
+                    connect: {
+                        id: roomId ? getTo.id: toId
+                    }
+                },
+                room:{
+                    connect:{
+                        id:room.id
+                    }
+                }
+            });
+
+        }
+    }
+}
