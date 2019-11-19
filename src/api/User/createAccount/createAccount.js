51dacabab2e7c1348b73bd37a9502cd41561ab59
jeonghunSakong <sgjh4040@naver.com>
2019-11-19 13:17:15 +0900

    edit createAccount method

--- src/api/User/createAccount/createAccount.js
+++ src/api/User/createAccount/createAccount.js
@@ -2,17 +2,32 @@ import { prisma } from "../../../../generated/prisma-client"
 
 
 export default {
-    Mutation:{
-        createAccount: async (_,args)=>{
-            const {username, email, firstName="", lastName="", bio=""}=args
-            const user = await prisma.createUser({
+    Mutation: {
+        createAccount: async (_, args) => {
+            const { username, email, firstName = "", lastName = "", bio = "" } = args;
+            const exist = await prisma.$exists.user({
+                OR:[
+                    {
+                        username
+                    },
+                    {
+                        email
+                    }
+                ]
+            });
+            if(exist){
+                throw Error("이름 또는 이메일이 이미 존재합니다.")
+            }
+            await prisma.createUser({
                 username,
                 email,
                 firstName,
                 lastName,
                 bio
             });
-            return user;
+            return true
+
+
         }
     }
 }
