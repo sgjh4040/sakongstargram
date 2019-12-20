62488198b600a241f29f9e96fdee117c3bf52e1a
jeonghunSakong <sgjh4040@naver.com>
2019-12-20 15:43:59 +0900

    change seeeUser para username to id

--- src/api/User/searchById/searchById.js
+++ src/api/User/searchById/searchById.js
@@ -1,8 +1,7 @@
 import { prisma } from "../../../../generated/prisma-client";
 export default {
   Query: {
-    seeUser: async (_, args) => {
-      console.log("seeUser");
+    searchById: async (_, args) => {
       const { id } = args;
       
       return prisma.user({ id });
