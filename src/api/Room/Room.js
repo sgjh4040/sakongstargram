1403ef8e768f97de198351c83c3294bf4c55e8ec
jeonghunSakong <sgjh4040@naver.com>
2020-01-08 18:25:05 +0900

    update chatroom unread

--- src/api/Room/Room.js
+++ src/api/Room/Room.js
@@ -3,6 +3,33 @@ import { prisma } from "../../../generated/prisma-client";
 export default {
   Room: {
     participants: ({ id }) => prisma.room({ id }).participants(),
-    messages: ({ id }) => prisma.room({ id }).messages()
+    messages: ({ id }) => prisma.room({ id }).messages(),
+    unReadCount: async (parent, _, { request }) => {
+      const { user } = request;
+      const { id: parentId } = parent;
+      try{
+        return prisma
+        .messagesConnection({where:{
+          AND:[
+            {
+              room:{id:parentId}
+            },
+            {
+              to: {id:user.id}
+            },
+            {
+              readYn: false
+            }
+          ]
+        }})
+        .aggregate()
+        .count()
+
+      }catch{
+        return 0;
+      }
+
+    }
+    
   }
 };
